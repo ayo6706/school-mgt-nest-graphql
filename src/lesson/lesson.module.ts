864216543129c3299bfd11e8d64254d904a2c6df
ayo6706 <ayo6706@gmail.com>
2023-09-27 02:24:51 +0100

    implement creatLesson service

--- src/lesson/lesson.module.ts
+++ src/lesson/lesson.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { LessonResolver } from './lesson.resolver';
 import { LessonService } from './lesson.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lessom.entity';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([Lesson])
+    ],
     providers: [LessonResolver, LessonService]
 })
 export class LessonModule {}
--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Lesson } from './lessom.entity';
+import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
-export class LessonService {}
+export class LessonService {
+    constructor(
+        @InjectRepository(Lesson) private repo: Repository<Lesson>
+    ){}
+
+    async createLesson(name: string, startDate: string, endDate: string): Promise<Lesson>{
+        const lesson = this.repo.create({
+            id: uuid(),
+            name,
+            startDate,
+            endDate
+        })
+
+        return this.repo.save(lesson);
+    }
+}
