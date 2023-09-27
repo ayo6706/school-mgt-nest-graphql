477edefe552c8a32cd5529d2b97cb64a70cc2e24
ayo6706 <ayo6706@gmail.com>
2023-09-27 01:58:34 +0100

    setup mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { LessonModule } from './lesson/lesson.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lessom.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities:  [Lesson]
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true
