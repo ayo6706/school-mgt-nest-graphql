48c49fe6b3be1ec54fcef6294849a88500d458b4
ayo6706 <ayo6706@gmail.com>
2023-10-01 05:26:03 +0100

    implement createStudent service

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -2,13 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Student } from './student.entity';
 import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { CreateStudentInput } from './student.input';
 
 @Injectable()
 export class StudentService {
     constructor(
-        @InjectRepository(Student) private repo: Repository<Student> 
-    ){
-        
+        @InjectRepository(Student) private repo: Repository<Student>
+    ){}
 
+    async createStudent(createStudentInput: CreateStudentInput){
+        const {firstName, lastName} =createStudentInput;
+
+        const student = await this.repo.create(
+            {
+                id: uuid(), 
+                firstName,
+                lastName
+            }
+        )
     }
 }
