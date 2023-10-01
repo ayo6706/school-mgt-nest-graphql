318a807d389573d7a63306a0320484e8809c8518
ayo6706 <ayo6706@gmail.com>
2023-10-01 08:44:40 +0100

    implement asign lesson to students resolver

---
+++ src/lesson/assign-students-to-lesson.input.ts
@@ -0,0 +1,10 @@
+import { Field, ID, InputType } from "@nestjs/graphql";
+
+@InputType()
+export class AssignStudentsToLessonInput{
+    @Field(type => [ID])
+    lessonID: string;
+
+    @Field(type => ID)
+    studentIDs: string[]
+}
--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -2,6 +2,7 @@ import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { LessonType } from "./lesson.type";
 import { LessonService } from "./lesson.service";
 import { CreateLessonInput } from "./create-lesson.input";
+import { AssignStudentsToLessonInput } from "./assign-students-to-lesson.input";
 
 @Resolver(of => LessonType)
 export class LessonResolver{
@@ -24,5 +25,15 @@ export class LessonResolver{
         @Args('createLessonInput') createLessonInput: CreateLessonInput,
         ){
         return this.service.createLesson(createLessonInput)
+    }
+
+    @Mutation(returns => LessonType)
+    assignStudentsToLesson(
+        @Args('assignStudentsToLessonInput') assignStudentsToLessonInput: AssignStudentsToLessonInput
+    ){
+        const {lessonID, studentIDs} = assignStudentsToLessonInput
+
+        return this.service.assignStudentsToLesson(lessonID, studentIDs)
+
     }
 }
