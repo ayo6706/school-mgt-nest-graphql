039a11ef6b5f7f339ceaaeddac3bf6268d9c43d9
ayo6706 <ayo6706@gmail.com>
2023-10-01 05:15:02 +0100

    inject repo to student service

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Student } from './student.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+    constructor(
+        @InjectRepository(Student) private repo: Repository<Student> 
+    ){
+        
+
+    }
+}
